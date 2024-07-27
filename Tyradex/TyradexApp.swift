2d56d45bce54401a0da6cb946c26a750a3520eed
Theo Sementa <theosementa@gmail.com>
2024-07-27 11:13:06 +0200

    Initial Commit

---
+++ Tyradex/TyradexApp.swift
@@ -0,0 +1,17 @@
+//
+//  TyradexApp.swift
+//  Tyradex
+//
+//  Created by KaayZenn on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TyradexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
